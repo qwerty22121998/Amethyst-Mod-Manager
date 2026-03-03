ef439df9463890e2dc45a301af99db44b98bc772
ChrisDKN <Chris.dkn@hotmail.com>
2026-03-03 22:10:09 +0000

    fixed packages not installing correctly

--- src/run.sh
+++ src/run.sh
@@ -2,13 +2,13 @@
 cd "$(dirname "$0")"
 
 if [ ! -d .venv ]; then
-    python3 -m venv .venv
+    python3 -m venv --system-site-packages .venv
     [ -f requirements.txt ] && .venv/bin/pip install -r requirements.txt -q
 fi
 
 # Install any missing or newly added requirements
 if [ -f requirements.txt ]; then
-    .venv/bin/pip install -r requirements.txt -q --disable-pip-version-check 2>/dev/null
+    .venv/bin/pip install -r requirements.txt -q --disable-pip-version-check
 fi
 
 .venv/bin/python3 gui.py "$@"
